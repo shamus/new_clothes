04d5c3af82637d79c9270b1b6cb9a8ab5b022456
Jeremy <jeremy@sidereel.com>
2012-10-15 15:35:30 -0700

    Reworking helper methods for creating persistent and domain classes on the fly in specs.

--- spec/lib/new_clothes/model_spec.rb
+++ spec/lib/new_clothes/model_spec.rb
@@ -4,19 +4,19 @@ describe NewClothes::Model do
   describe "when included" do
     context "by default" do
       it "finds the corresponding persistent model in the Persistence namespace" do
-        define_persistent_model_in_namespace :foo, :Persistence
-        define_domain_model_in_namespace :foo, :Domain
+        in_namespace(:Persistence) { define_persistent_model :foo }
+        in_namespace(:Domain) { define_domain_model :foo }
 
         Domain::Foo.persistent_model.should == Persistence::Foo
-
-        remove_namespaces :Persistence, :Domain
       end
 
       context "with attributes" do
-        let(:persistent_model) { define_persistent_model(:foo, :a => :string, :a_count => :integer) }
-        let(:domain_model) { define_domain_model persistent_model }
+        before do
+          in_namespace(:Persistence) { define_persistent_model :foo, :a => :string, :a_count => :integer }
+          in_namespace(:Domain) { define_domain_model :foo }
 
-        before { @domain_instance = domain_model.new persistent_model.new }
+          @domain_instance = Domain::Foo.new(Persistence::Foo.new)
+        end
 
         it "exposes a hash of the persistent model's content attributes" do
           @domain_instance.attributes.keys.should =~ ["id", "a"]
@@ -26,39 +26,39 @@ describe NewClothes::Model do
           @domain_instance.should respond_to(:id)
           @domain_instance.should respond_to(:a)
           @domain_instance.should_not respond_to(:a_count)
-       end
+        end
       end
     end
 
     context "when the persistent namespace is configured" do
       it "finds the corresponding persistent model in the configured namespace" do
         NewClothes.persistent_namespace = "Configured"
-        define_persistent_model_in_namespace :foo, :Configured
-        define_domain_model_in_namespace :foo, :Domain
+        in_namespace(:Configured) { define_persistent_model :foo }
+        in_namespace(:Domain) { define_domain_model :foo }
 
         Domain::Foo.persistent_model.should == Configured::Foo
-
-        remove_namespaces :Configured, :Domain
       end
     end
 
     context "when the persistent model can't be found" do
       specify do
-        expect { define_domain_model.persistent_model }.to raise_exception
+        expect do
+          in_namespace(:Domain) { define_domain_model.persistent_model }
+        end.to raise_exception
       end
     end
   end
 
   describe "specifying the persitent class" do
     it "does not attempt to look up the persistent model in the configured namespace" do
-      persistent_model = define_persistent_model :foo
+      in_namespace(:Specified) { define_persistent_model :foo }
       domain_model = Class.new do
         include NewClothes::Model
 
-        set_persistent_model persistent_model
+        set_persistent_model Specified::Foo
       end
 
-      domain_model.persistent_model.should == persistent_model
+      domain_model.persistent_model.should == Specified::Foo
     end
   end
 end
---
+++ spec/support/new_clothes_helpers.rb
@@ -0,0 +1,75 @@
+module NewClothesHelpers
+  def self.register_constant_for_removal constant
+    constants_for_removal << constant
+  end
+
+  def self.remove_defined_constants
+    constants_for_removal.delete_if do |constant|
+      Object.class_eval { remove_const constant.name }
+    end
+  end
+
+  def self.constants_for_removal
+    @constants_for_removal ||= []
+  end
+
+  class ModelBuilder
+    def initialize namespace
+      @namespace = namespace
+    end
+
+    def define_persistent_model name, attributes = {}, &block
+      name = name.to_s.underscore.to_sym
+
+      ActiveRecord::Base.connection.schema_cache.clear!
+      ActiveRecord::Base.connection.create_table name, :force => true do |t|
+        attributes.each { |name, type| t.column name, type }
+      end
+
+      persistent_model = Class.new(ActiveRecord::Base) do
+        self.table_name = name
+      end
+
+      persistent_model.instance_eval &block if block_given?
+      define_constant name, persistent_model
+    end
+
+    def define_domain_model name, persistent_model = nil
+      domain_model = Class.new do
+        include NewClothes::Model
+        set_persistent_model persistent_model if persistent_model
+
+        def initialize model
+          @model = model
+        end
+
+        attr_reader :model
+      end
+
+      define_constant name, domain_model
+    end
+
+    private
+    attr_reader :namespace
+
+    def define_constant name, value
+      name = name.to_s.classify.to_sym
+      namespace.const_set name, value
+    end
+  end
+
+  def in_namespace name, &block
+    name = name.to_s.classify.to_sym
+    constant = Object.const_set name, Module.new
+    NewClothesHelpers.register_constant_for_removal constant
+
+    ModelBuilder.new(constant).instance_eval &block
+  end
+end
+
+RSpec.configure do |c|
+  c.include NewClothesHelpers
+  c.after(:each) do
+    NewClothesHelpers.remove_defined_constants
+  end
+end
