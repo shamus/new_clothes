509f079a3bde2fe5695c692aac5e15773549f76f
Jeremy <jeremy@sidereel.com>
2012-10-17 12:22:01 -0400

    Removing vestigal load path set up from spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'new_clothes'
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), 'support', "models")
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each do |f|
   require f
 end
